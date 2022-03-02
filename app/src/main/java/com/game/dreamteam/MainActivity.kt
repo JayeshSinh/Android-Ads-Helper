6c0b69c7e90d5cadc577a2f8749bc30f72cd5697
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2022-03-02 17:54:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/game/dreamteam/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.game.dreamteam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.ads.helper.InterstitialAdHelper
+import com.example.app.ads.helper.InterstitialAdHelper.isShowInterstitialAd
+import com.example.app.ads.helper.developerNeedLoadAdsOnce
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        InterstitialAdHelper.loadInterstitialAd(this@MainActivity)
+
+        btn1.setOnClickListener {
+            isShowInterstitialAd {
+
+            }
+        }
+
+        btn2.setOnClickListener {
+            developerNeedLoadAdsOnce = true
+            InterstitialAdHelper.loadInterstitialAd(this@MainActivity){
+                isShowInterstitialAd {
+
+                }
+            }
+        }
+
+        btn3.setOnClickListener {
+            developerNeedLoadAdsOnce = false
+            InterstitialAdHelper.loadInterstitialAd(this@MainActivity){
+                isShowInterstitialAd {
+
+                }
+            }
+        }
+    }
+}
